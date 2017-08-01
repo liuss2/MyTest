d7d3e078c60a189fd329044d0661668c518c7106
lss <1522275652@qq.com>
2017-08-01 11:19:52 +0800

    测试

---
+++ TestDemo/TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by lss on 2017/8/1.
+//  Copyright © 2017年 lss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by lss on 2017/8/1.
+//  Copyright © 2017年 lss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
